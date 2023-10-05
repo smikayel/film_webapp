ec0095014fde70090d0bc5fd6c48a758e6a93ad2
smikayel <sasha-mikayelyan@mail.ru>
2023-10-06 01:34:17 +0400

    feature: vertical card design

--- frontend/src/components/featured/section/index.tsx
+++ frontend/src/components/featured/section/index.tsx
@@ -1,6 +1,7 @@
 import { Chip } from "../../shared/chip/chip";
 import { SearchBar } from "../../shared/search";
 import { Title } from "../../shared/title";
+import { Card } from "../../shared/verticalCard";
 
 const chips = [
   {
@@ -12,6 +13,20 @@ const chips = [
     iconSrc: "/icons/tv.svg",
   },
 ];
+
+const mockFilm = {
+  id: 0,
+  title: "KING KONG",
+  image: "https://via.placeholder.com/224x332",
+  director: "string",
+  genres: "Action, Adventure, Drama",
+  duration: 187,
+  score: "",
+  rating: 7.62,
+  overview: "string",
+  year: 2005,
+  actors: "string",
+};
 
 export function Section() {
   return (
@@ -27,6 +42,7 @@ export function Section() {
           ))}
         </div>
       </div>
+      <Card film={mockFilm} />
     </div>
   );
 }
